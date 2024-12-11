ddb6ad59f3535657519dcdcf6194c0b7d9ce0a04
Narongrit2544 <Boomza5342144@gmail.com>
2024-12-11 11:43:54 +0700

    Update Report Page

--- src/data/teachersTableData.js
+++ src/data/teachersTableData.js
@@ -1,17 +1,15 @@
 export const teachersTableData = [
     {
       img: "https://www.ce.kmitl.ac.th/api/faculty/download/Kiatnarong", // รูปภาพตัวแทน
-      name: "Dr. Kiatnarong Tongprasert",
+      name: "Dr.Kiatnarong Tongprasert",
       email: "kiatnarong.to@kmitl.ac.th",
-      department: "Computer Engineering",
       online: true,
       date: "14/09/20",
     },
     {
       img: "https://www.ce.kmitl.ac.th/api/faculty/download/Thana",
-      name: "Dr. Thana Hongsuwan",
+      name: "Dr.Thana Hongsuwan",
       email: "khthana@kmitl.ac.th",
-      department: "Computer Engineering",
       online: false,
       date: "14/09/20",
     },
