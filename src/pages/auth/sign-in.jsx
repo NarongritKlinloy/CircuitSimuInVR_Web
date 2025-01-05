1d8ffd77646e6af2a8b0dedf7acd4d8947c26bc0
Narongrit2544 <Boomza5342144@gmail.com>
2025-01-05 14:26:06 +0700

    update login page

--- src/pages/auth/sign-in.jsx
+++ src/pages/auth/sign-in.jsx
@@ -1,8 +1,11 @@
+import {
+  Typography,
+} from "@material-tailwind/react";
+import { useNavigate } from "react-router-dom";
 import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
 import Swal from "sweetalert2";
-import { useNavigate } from "react-router-dom";
 
-export function SignIn({ backgroundImage }) {
+export function SignIn() {
   const navigate = useNavigate();
 
   const handleGoogleLoginSuccess = (credentialResponse) => {
@@ -20,14 +23,28 @@ export function SignIn({ backgroundImage }) {
       return;
     }
 
-    Swal.fire({
-      icon: "success",
-      title: "Login Successful",
-      text: `Welcome, ${email}`,
-      confirmButtonText: "OK",
-    }).then(() => {
-      navigate("/dashboard/home");
-    });
+    const usernamePart = email.split("@")[0];
+    const isNumber = /^\d{8}$/;
+
+    if (isNumber.test(usernamePart)) {
+      Swal.fire({
+        icon: "error",
+        title: "Access Denied",
+        text: "You are not authorized to access the admin system.",
+        confirmButtonText: "OK",
+      }).then(() => {
+        navigate("/dashboard/home");
+      });
+    } else {
+      Swal.fire({
+        icon: "success",
+        title: "Login Successful",
+        text: `Welcome, Teacher: ${email}`,
+        confirmButtonText: "OK",
+      }).then(() => {
+        navigate("/teacher/home");
+      });
+    }
   };
 
   const handleGoogleLoginError = () => {
@@ -41,74 +58,67 @@ export function SignIn({ backgroundImage }) {
 
   return (
     <GoogleOAuthProvider clientId="289166698407-3sivu8jo52aveuuh78kg8n17agseta42.apps.googleusercontent.com">
-      <div
-        className="min-h-screen bg-cover bg-center flex flex-col justify-center sm:py-12"
-        style={{
-          backgroundImage: `url('${backgroundImage || "https://img2.pic.in.th/pic/2212.w019.n002.800B.p15.800.jpg"}')`,
-        }}
-      >
-        <div className="relative py-3 sm:max-w-xl sm:mx-auto">
-          <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-sky-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
-          <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
-            <div className="max-w-md mx-auto">
-              <div>
-                <h1 className="text-2xl font-semibold text-gray-900">Login</h1>
-              </div>
-              <div className="divide-y divide-gray-200">
-                <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
-                  <div className="w-full flex justify-center">
-                    <GoogleLogin
-                      onSuccess={handleGoogleLoginSuccess}
-                      onError={handleGoogleLoginError}
-                      render={(renderProps) => (
-                        <button
-                          onClick={renderProps.onClick}
-                          disabled={renderProps.disabled}
-                          className="flex items-center bg-white border border-gray-300 rounded-lg shadow-md px-6 py-2 text-sm font-medium text-gray-800 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-                        >
-                          <svg
-                            className="h-6 w-6 mr-2"
-                            xmlns="http://www.w3.org/2000/svg"
-                            xmlnsXlink="http://www.w3.org/1999/xlink"
-                            viewBox="-0.5 0 48 48"
-                            version="1.1"
-                          >
-                            <g id="Icons" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-                              <g id="Color-" transform="translate(-401.000000, -860.000000)">
-                                <g id="Google" transform="translate(401.000000, 860.000000)">
-                                  <path
-                                    d="M9.82727273,24 C9.82727273,22.4757333 10.0804318,21.0144 10.5322727,19.6437333 L2.62345455,13.6042667 C1.08206818,16.7338667 0.213636364,20.2602667 0.213636364,24 C0.213636364,27.7365333 1.081,31.2608 2.62025,34.3882667 L10.5247955,28.3370667 C10.0772273,26.9728 9.82727273,25.5168 9.82727273,24"
-                                    fill="#FBBC05"
-                                  ></path>
-                                  <path
-                                    d="M23.7136364,10.1333333 C27.025,10.1333333 30.0159091,11.3066667 32.3659091,13.2266667 L39.2022727,6.4 C35.0363636,2.77333333 29.6954545,0.533333333 23.7136364,0.533333333 C14.4268636,0.533333333 6.44540909,5.84426667 2.62345455,13.6042667 L10.5322727,19.6437333 C12.3545909,14.112 17.5491591,10.1333333 23.7136364,10.1333333"
-                                    fill="#EB4335"
-                                  ></path>
-                                  <path
-                                    d="M23.7136364,37.8666667 C17.5491591,37.8666667 12.3545909,33.888 10.5322727,28.3562667 L2.62345455,34.3946667 C6.44540909,42.1557333 14.4268636,47.4666667 23.7136364,47.4666667 C29.4455,47.4666667 34.9177955,45.4314667 39.0249545,41.6181333 L31.5177727,35.8144 C29.3995682,37.1488 26.7323182,37.8666667 23.7136364,37.8666667"
-                                    fill="#34A853"
-                                  ></path>
-                                  <path
-                                    d="M46.1454545,24 C46.1454545,22.6133333 45.9318182,21.12 45.6113636,19.7333333 L23.7136364,19.7333333 L23.7136364,28.8 L36.3181818,28.8 C35.6879545,31.8912 33.9724545,34.2677333 31.5177727,35.8144 L39.0249545,41.6181333 C43.3393409,37.6138667 46.1454545,31.6490667 46.1454545,24"
-                                    fill="#4285F4"
-                                  ></path>
-                                </g>
-                              </g>
-                            </g>
-                          </svg>
-                          <span>Continue with Google</span>
-                        </button>
-                      )}
-                    />
-                  </div>
-                </div>
+       
+       <section className="flex flex-wrap items-center justify-center min-h-screen bg-gray-300"> {/* เปลี่ยนสีพื้นหลัง */}
+        <div className="w-full lg:w-1/2 p-8">
+          {/* กล่องหลัก */}
+
+          {/* เพิ่มรูปโลโก้ */}
+          <div className="flex justify-center mt-4 py-5 px-6">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/KMITL_Sublogo.svg/768px-KMITL_Sublogo.svg.png" // ลิงก์ของโลโก้
+              alt="Logo"
+              className="w-64 h-auto" // ปรับขนาดโลโก้
+            />
+            <img
+              src="https://img5.pic.in.th/file/secure-sv1/DALLE-2025-01-05-14.14.45---A-bright-and-futuristic-circular-logo-design-for-a-virtual-reality-VR-application-focused-on-circuit-building.-The-logo-features-a-sleek-VR-headset.png" // ลิงก์ของโลโก้
+              alt="Logo"
+              className="w-32 h-auto" // ปรับขนาดโลโก้
+            />
+          </div>
+
+
+          <div className="bg-white border border-gray-300 shadow-lg rounded-lg overflow-hidden">
+            {/* ส่วนหัวสีส้ม */}
+            <div className="bg-orange-500 text-white py-4 px-6">
+              <h1 className="text-lg font-bold text-center">KMITL Registration System</h1>
+            </div>
+
+
+            {/* เนื้อหาด้านใน */}
+            <div className="p-6 text-center">
+              <Typography variant="h3" className="font-bold text-gray-800">
+                ยืนยันตัวตนด้วยบริการของ Google
+              </Typography>
+              <Typography
+                variant="paragraph"
+                color="blue-gray"
+                className="text-lg font-normal mt-2"
+              >
+                โดยใช้ Email Account ของสถาบันฯ
+              </Typography>
+              <div className="max-w-md mx-auto mt-6">
+                <GoogleLogin
+                  onSuccess={handleGoogleLoginSuccess}
+                  onError={handleGoogleLoginError}
+                />
               </div>
             </div>
           </div>
         </div>
-      </div>
-      
+
+
+        <div className="hidden lg:block w-full lg:w-1/2">
+          <img
+            //src="https://img2.pic.in.th/pic/DALLE-2024-12-23-12.47.png"
+            src="https://img2.pic.in.th/pic/DALLE-2025-01-05-14.21.58---A-person-wearing-a-sleek-and-modern-VR-headset-with-a-distinctively-Thai-cultural-twist.-The-individual-is-dressed-in-a-contemporary-Thai-inspired-out.jpg"
+            alt="Login Illustration"
+            className="w-full h-full object-cover rounded-lg"
+          />
+        </div>
+      </section>
     </GoogleOAuthProvider>
+
   );
 }
 
