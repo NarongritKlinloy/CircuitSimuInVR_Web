4ac83fe5f4c20c0da10b88a09dfb05160758d951
OsisZool <jbmuthita@gmail.com>
2025-01-19 00:25:28 +0700

    change localhost password

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Dream241244",
+  password: "xxx",
   database: "project_circuit",
 });
 
