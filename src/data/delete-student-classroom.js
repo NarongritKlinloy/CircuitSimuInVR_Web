748d9344efa0a8ee7a53f11e411f03fb937383e9
OsisZool <jbmuthita@gmail.com>
2025-02-11 18:10:06 +0700

    edit del student api (client)

--- src/data/delete-student-classroom.js
+++ src/data/delete-student-classroom.js
@@ -4,14 +4,16 @@ export const deleteStudentAPI = async (uid, class_id) => {
     try {
         const response = await axios.delete(`http://localhost:5000/api/classroom/student/${uid}/${class_id}`);
         // console.log(response.data.message);
-        Swal.fire({
-            title: "Deleted!",
-            text: `${uid} has been deleted.`,
-            icon: "success", confirmButtonText: "OK",
-            customClass: {
-                confirmButton: 'bg-blue-500 text-white rounded px-4 py-2 hover:bg-blue-600',
-            }
-        });
+        if(response.status === 200){
+            Swal.fire({
+                title: "Deleted!",
+                text: `${uid} has been deleted.`,
+                icon: "success", confirmButtonText: "OK",
+                customClass: {
+                    confirmButton: 'bg-blue-500 text-white rounded px-4 py-2 hover:bg-blue-600',
+                }
+            });
+        }
     } catch (error) {
         console.error("Error deleting student of classroom:", error);
         Swal.fire({
