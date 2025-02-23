6b9ca0ba42cb930d523252a08472ec715521d5e3
Aphidech <125728661+ChampAphidech@users.noreply.github.com>
2025-02-23 21:44:51 +0700

    change title Recommend to feedback

--- src/pages/dashboard/notifications.jsx
+++ src/pages/dashboard/notifications.jsx
@@ -135,7 +135,7 @@ const filteredReports = reports.filter((report) => {
      
       <Card>
         <CardHeader variant="gradient" color="gray" className="mb-8 p-6 flex justify-between items-center">
-          <Typography variant="h6" color="white">Recommendation</Typography>
+          <Typography variant="h6" color="white">Feedback</Typography>
         </CardHeader>
         <CardBody className="overflow-x-auto overflow-y-auto max-h-96 pt-0 pb-2">
           <table className="w-full min-w-[640px] table-auto border-collapse">
