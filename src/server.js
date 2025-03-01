258841789d0d62a754887d14b76b69ff4278a7bd
OsisZool <jbmuthita@gmail.com>
2025-03-01 18:34:30 +0700

    show practice score (Teacher)

--- src/server.js
+++ src/server.js
@@ -560,8 +560,7 @@ app.get("/api/classroom/practice/:class_id/:practice_id", async (req, res) => {
                               ) AS max_scores
                                   ON ps.uid = max_scores.uid AND ps.score = max_scores.max_score
                               WHERE cp.class_id = ? 
-                                AND cp.practice_id = ?;
-`;
+                                AND cp.practice_id = ?`;
   try {
     const [rows] = await db.query(sql_practice_score, [class_id, practice_id]);
     if (rows.length === 0) {
