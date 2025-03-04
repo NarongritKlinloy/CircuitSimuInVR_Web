c0f61b61dcf003a19a0448ffa9ed45412f2b1101
Narongrit2544 <125728497+Narongrit2544@users.noreply.github.com>
2025-03-04 22:37:06 +0700

    test

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,25 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
   },
+
   build: {
     target: "esnext", // รองรับ top-level await
   },
+
   server: {
-    host: true,   // เปิดให้เข้าถึงจากภายนอก (เช่นผ่าน IP)
-    port: 3000    // กำหนดให้ใช้พอร์ต 5173
+    host: true,   // เปิดให้เข้าถึงจาก IP ภายนอก (0.0.0.0)
+    port: 3000    // พอร์ต dev server (ถ้าอยากใช้ 5173 ก็ปรับตามต้องการ)
   },
+
   preview: {
-    host: true,   // เปิดให้เข้าถึงจากภายนอกในโหมด preview
-    port: 3000   // กำหนดพอร์ตให้ตรงกับ `docker-compose.yml`
-  }
+    host: true,   
+    port: 3000,   // พอร์ตโหมด preview
+    allowedHosts: ["smith11.ce.kmitl.ac.th"] // อนุญาตให้เข้าถึงจากโดเมนนี้
+    // หรือหากต้องการเปิดกว้างทุกโฮสต์ ให้ใส่ 'all'
+    // allowedHosts: 'all'
+  },
 });
